b28d32fcdc03f2d4aa582fe14d3555f053b030b9
Ajin892 <ajinzcbg@gmail.com>
2026-08-18 20:36:49 +0530

    Add SQL queries for data retrieval and manipulation in homework.sql

--- homework.sql
+++ homework.sql
@@ -10,3 +10,21 @@ INSERT INTO books (id, title, author, price, stock) VALUES
 (3, 'The Psychology of Money', 'Morgan Housel', 400, 30),
 (4, 'Ikigai', 'Francesc Miralles', 300, 60),
 (5, 'Deep Work', 'Cal Newport', 500, 20);
+SELECT *
+FROM books
+WHERE price < 450
+  AND stock > 30;
+ UPDATE books
+SET stock = 45,
+    price = 420
+WHERE title = 'Deep Work';
+DELETE FROM books
+WHERE title = 'Ikigai';
+SELECT
+    AVG(price) AS average_book_price,
+    COUNT(*) AS total_books
+FROM books;
+SELECT *
+FROM books
+ORDER BY price DESC
+LIMIT 3;
