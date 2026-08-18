c31bd0479674705bdd74680be48c2e1c47468cd9
Ajin892 <ajinzcbg@gmail.com>
2026-08-18 20:09:47 +0530

    first commit

---
+++ classwork.sql
@@ -0,0 +1,24 @@
+INSERT INTO `mobile store`(`ID`, `brand`, `model`, `price`, `stock`)
+VALUES
+(1, 'Samsung', 'Galaxy M14', 12000, 30),
+(2, 'Redmi', 'Note 12', 15000, 25),
+(3, 'Realme', 'Narzo 50', 13000, 20),
+(4, 'Samsung', 'Galaxy A23', 18000, 10);
+SELECT *
+FROM `mobile store`
+WHERE price > 13000 OR stock < 15;
+UPDATE `mobile store`
+SET stock = stock + 5,
+    price = 12500
+WHERE model = 'Narzo 50';
+DELETE FROM `mobile store`
+WHERE id = 2;
+SELECT MIN(price) AS lowest_price,
+       MAX(price) AS highest_price
+FROM `mobile store`;
+SELECT SUM(stock) AS total_stock
+FROM `mobile store`;
+SELECT *
+FROM * `mobile store`
+ORDER BY price DESC
+LIMIT 2;
