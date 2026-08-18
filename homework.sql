49b9097000ab8d22446ee4af6ca19e398b4d49f1
Ajin892 <ajinzcbg@gmail.com>
2026-08-18 20:33:31 +0530

    Add initial data entries for books in homework.sql

---
+++ homework.sql
@@ -0,0 +1,12 @@
+INSERT INTO books (id, title, author, price, stock) VALUES
+(1, 'The Alchemist', 'Paulo Coelho', 350, 50),
+(2, 'Atomic Habits', 'James Clear', 450, 40),
+(3, 'The Psychology of Money', 'Morgan Housel', 400, 30),
+(4, 'Ikigai', 'Francesc Miralles', 300, 60),
+(5, 'Deep Work', 'Cal Newport', 500, 20);
+INSERT INTO books (id, title, author, price, stock) VALUES
+(1, 'The Alchemist', 'Paulo Coelho', 350, 50),
+(2, 'Atomic Habits', 'James Clear', 450, 40),
+(3, 'The Psychology of Money', 'Morgan Housel', 400, 30),
+(4, 'Ikigai', 'Francesc Miralles', 300, 60),
+(5, 'Deep Work', 'Cal Newport', 500, 20);
